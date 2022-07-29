5df306cc2f3b2d709403dec6dd0429b2e1623104
Nikhil Paleti <paletinikhil@gmail.com>
2022-07-30 00:15:34 +0530

    Minor touch up
    Cleaned up and structured the code, added comments, minor optimizations

--- test.py
+++ test.py
@@ -20,12 +20,12 @@ import matplotlib.pyplot as plt
 # dynamodb_partition_key = ''
 # dynamodb_table_name = ''
 
-
 twt_api = twitter.Api(consumer_key=config.twitter_consumer_key, consumer_secret=config.twitter_consumer_secret, access_token_key=config.twitter_access_token_key, access_token_secret=config.twitter_access_token_secret)
 
 aws_api_comprehend = boto3.client(service_name = 'comprehend', aws_access_key_id = config.aws_access_key_id, aws_secret_access_key = config.aws_secret_access_key, region_name = 'us-east-1')
-# aws_api_dynamo = boto3.client(service_name = 'dynamodb', aws_access_key_id = config.aws_access_key_id, aws_secret_access_key = config.aws_secret_access_key, region_name = 'us-east-1')
+# aws_api_s3 = boto3.client(service_name = 's3', aws_access_key_id = config.aws_access_key_id, aws_secret_access_key = config.aws_secret_access_key, region_name = 'us-east-1')
 aws_api_dynamo = boto3.resource('dynamodb', aws_access_key_id = config.aws_access_key_id, aws_secret_access_key = config.aws_secret_access_key, region_name = 'us-east-1')
+
 # Headers/Parameters for the request
 headers = {
         'Authorization': 'Bearer {}'.format(config.twitter_bearer_token),
@@ -34,6 +34,8 @@ headers = {
 params = (
     ('tweet.fields', 'context_annotations'),
 )
+
+# Payload adds filters for the stream
 payload = {
     'add':[
         {
@@ -41,8 +43,9 @@ payload = {
         }
     ]
 }
-response = requests.post('https://api.twitter.com/2/tweets/search/stream/rules', headers=headers, json=payload)
 
+# Checking if Twitter API is working and giving desired response
+response = requests.post('https://api.twitter.com/2/tweets/search/stream/rules', headers=headers, json=payload)
 if response.status_code == 201:
     print('Response: {}'.format(response.text))
 else:
@@ -55,21 +58,10 @@ neu_list = []
 mix_list = []
 id_list = []
 
-
 fig= plt.figure()
-# ax1 = fig.add_subplot(1,1,1)
-plt.ion()
+plt.ion() #Key to live graph updating
 
-# def animate():
-#     ax1.clear()
-#     ax1.plot(pos_list, color = "green")
-#     ax1.plot(neg_list, color = "red")
-#     ax1.plot(neu_list, color = "blue")
-#     ax1.plot(mix_list, color = "black")
-
-
-
-def stream_connect(bearer_token):
+def process(bearer_token):
     
     response = requests.get('https://api.twitter.com/2/tweets/search/stream', headers=headers, stream=True)
     
@@ -77,24 +69,27 @@ def stream_connect(bearer_token):
         if (new_line):
             tweet = json.loads(new_line)
             tweet_id = tweet['data']['id']
-            # id_list.append(tweet_id)
-            text = tweet['data']['text']
-            tweet_sentiment = aws_api_comprehend.detect_sentiment(Text = text, LanguageCode = 'en')
-            sentiment = tweet_sentiment['Sentiment']
-            sentiment_score = tweet_sentiment['SentimentScore']
-            for score in sentiment_score:
-                sentiment_score[score] = round(sentiment_score[score], 3)
+            tweet_text = tweet['data']['text']
+            aws_analysis = aws_api_comprehend.detect_sentiment(Text = tweet_text, LanguageCode = 'en')
+            tweet_sentiment = aws_analysis['Sentiment']
+            tweet_analysis = aws_analysis['SentimentScore']
+            # Need rounding. Breaks Matplotlib and slows down a lot. Its like 10 decimals or some shizz
+            for score in tweet_analysis:
+                tweet_analysis[score] = round(tweet_analysis[score], 2)
+            # tweet_id: Tweet ID
+            # tweet_text: Tweet Text 
+            # tweet_sentiment: Major Sentiment 
+            # tweet_analysis: Sentiment Breakdown
             
             global pos_list
             global neg_list
             global neu_list
             global mix_list
             
-            
-            pos_list.append(sentiment_score['Positive']) 
-            neg_list.append(sentiment_score['Negative'])
-            neu_list.append(sentiment_score['Neutral'])
-            mix_list.append(sentiment_score['Mixed'])    
+            pos_list.append(tweet_analysis['Positive']) 
+            neg_list.append(tweet_analysis['Negative'])
+            neu_list.append(tweet_analysis['Neutral'])
+            mix_list.append(tweet_analysis['Mixed'])    
             
             
             if (len(mix_list)>15):
@@ -105,46 +100,36 @@ def stream_connect(bearer_token):
                 neu_list = neu_list[no:] 
                 plt.pause(0.1) 
                 plt.cla()       
-            
-            # print('Tweet ID: ', tweet_id)
-            # print('Tweet: ', text) 
-            # print('Major Sentiment: ', sentiment)
-            # print('Sentiments: ', sentiment_score)
-            
-            
+             
+            # Adding Tweet data to DynamoDB
             table = aws_api_dynamo.Table(config.dynamodb_table_name)
             cols = ["Tweet", "MainSentiment", "Breakdown"]
 
             response = table.put_item(
                 Item = {
                     config.dynamodb_partition_key: str(tweet_id),
-                    cols [0]: str(text),
-                    cols [1]: str(sentiment) ,
-                    cols [2]: str(sentiment_score) 
+                    cols [0]: str(tweet_text),
+                    cols [1]: str(tweet_sentiment) ,
+                    cols [2]: str(tweet_analysis) 
                 }
             )
             
-            
+            # Them lovely live-graphs 
             plt.plot(pos_list, color = "green", label='Positive')
             plt.plot(neg_list, color = "red", label='Negative')
             plt.plot(neu_list, color = "blue", label='Neutral')
             plt.plot(mix_list, color = "green", label='Black')
             
             plt.legend()
-            
-            # time.sleep(0.2)
             plt.show()
-            # print (len(mix_list))
 
-# Final code to kick-off everything
- 
-BEARER_TOKEN = config.twitter_bearer_token
+# Final code to kick-off everything, i.e, stream
 while True:
-    stream_connect(BEARER_TOKEN)
+    process(config.twitter_bearer_token)
 
 
 #Experimenting with the upload to s3/Dynamo
-# bucket = 'letmepretendbucket'
+# bucket = ''
 # filename = 'file.csv'
 
 # aws_api_s3.Object(bucket, filename).delete()
